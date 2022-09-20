e580b3bbce926ac221ff8d1502cea0fbef6ffd67
Mojens <moha135h@stud.kea.dk>
2022-09-20 14:26:55 +0200

    commi

--- week2_day1/js/js2.js
+++ week2_day1/js/js2.js
@@ -6,23 +6,15 @@ div1.addEventListener("click", function () {
 })
 let div2 = document.getElementById("div2");
 
-div2.addEventListener("click", function () {
-    divParagraph.innerHTML = "div2 clicked";    
+div2.addEventListener("click", function () {   
     alert("div2 clicked");
+    divParagraph.innerHTML = "div2 clicked"; 
 })
 
 let divOuter = document.getElementById("outer");
-
-divOuter.onclick = function (event) {
-    event.target.style.backgroundColor = 'yellow';
-
-    // chrome needs some time to paint yellow
-    setTimeout(() => {
-        alert("target = " + event.target.tagName + ", this=" + this.tagName);
-        event.target.style.backgroundColor = ''
-    }, 0);
-    divParagraph.innerHTML = "target = " + event.target.tagName + ", this=" + this.tagName;
-};
-
-
+divOuter.addEventListener("click", function (e) {   
+    const target = e.target;
+alert("target = " + target.id + ", this=" + this.id);
+divParagraph.innerHTML = "target = " + target.id + ", this=" + this.id;
+});
 
