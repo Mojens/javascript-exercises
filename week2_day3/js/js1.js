3e0833195c97f59cc9d842883c2b11c97028b382
Mojens <moha135h@stud.kea.dk>
2022-09-21 10:00:09 +0200

    jsday3week2

--- week2_day3/js/js1.js
+++ week2_day3/js/js1.js
@@ -1,2 +1,20 @@
 const URL  = "https://jsonplaceholder.typicode.com/users/";
 
+const res = fetch(URL) //promise
+const res2 = res.then((r)=>{ // Promise -> Json
+    return r.json();
+})
+res2.then(data => { // JS Object
+    console.log(data);
+})
+
+//nemme måde 
+fetch(URL)
+.then((r)=>{
+    console.log(r.status);
+    return r.json();
+})
+.then(data =>{
+    console.log(data);
+})
+console.log("Who was called first")
