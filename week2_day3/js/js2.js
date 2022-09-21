9b913b5809fc3ac2a30c1eeca8b42d4d632cd54d
Mojens <moha135h@stud.kea.dk>
2022-09-21 20:07:06 +0200

    færdig med add

--- week2_day3/js/js2.js
+++ week2_day3/js/js2.js
@@ -1,4 +1,4 @@
-                                     /*     CARS'R'US      */                                                    
+/*     CARS'R'US      */
 let URL_MEMBER = "http://localhost:8080/api/members/";
 let URL_CAR = "http://localhost:8080/api/cars/";
 let URL_RESERVATION = "http://localhost:8080/api/reservation/";
@@ -38,7 +38,7 @@ function loadDataCar() {
             //Her kan vi arbejde med data
             const ul = carData.map(car => {
                 console.log(car)
-                return `<tr><td>${car.id}</td><td>${car.brand}</td><td>${car.model}</td><td>${car.pricePrDay}</td></tr>`
+                return `<tr><td>${car.id}</td><td>${car.brand}</td><td>${car.model}</td><td>${car.pricePrDay + " DKK"}</td></tr>`
             })
             const ulAsString = ul.join("")
             document.getElementById("tbody_cars").innerHTML = ulAsString
@@ -63,11 +63,12 @@ function loadDataCarById(carId) {
             return r.json()  //Returns a promise
         })
         .then(carData => {
-            const tr = `<tr><td>${carData.id}</td><td>${carData.brand}</td><td>${carData.model}</td><td>${carData.pricePrDay}</td></tr>`
+            const tr = `<tr><td>${carData.id}</td><td>${carData.brand}</td><td>${carData.model}</td><td>${carData.pricePrDay + " DKK"}</td></tr>`
             console.log(carData)
             document.getElementById("tbody_findcar").innerHTML = tr
         })
         .catch(e => {
+            document.getElementById("tbody_findcar").innerHTML = "<tr><td>No car found</td><td>No car found</td><td>No car found</td><td>No car found</td></tr>"
             console.error(e)
         })
 }
@@ -88,11 +89,61 @@ function loadDataMemberByUserName(memberId) {
         .then(memberData => {
             //Her kan vi arbejde med data
             const member = `<tr><td>${memberData.username}</td><td>${memberData.email}</td><td>${memberData.firstName + " " + memberData.lastName}</td></tr>`
-            
-                document.getElementById("tbody_findmember").innerHTML = member
-            })
+
+            document.getElementById("tbody_findmember").innerHTML = member
+        })
         .catch(e => {
+            document.getElementById("tbody_findmember").innerHTML = "<tr><td>No member found</td><td>No member found</td><td>No member found</td></tr>"
             console.error(e)
         })
 }
 document.getElementById("searchmemberbtn").onclick = () => loadDataMemberByUserName(memberId);
+
+
+/////////////////////////////////// ADD - POST METHODS///////////////////////////////////////////////////////////////
+
+function addMember() {
+    const createdMember = {
+        method: "POST",
+        headers: { "Content-type": "application/json" },
+        body: JSON.stringify({
+            username: document.getElementById("inputUserName").value,
+            email: document.getElementById("inputEmail").value,
+            password: document.getElementById("inputPassword").value,
+            firstName: document.getElementById("inputFirstName").value,
+            lastName: document.getElementById("inputLastName").value,
+            street: document.getElementById("inputStreet").value,
+            city: document.getElementById("inputCity").value,
+            zip: document.getElementById("inputZip").value
+        })
+    }
+    fetch(URL_MEMBER, createdMember)
+    .then(r => r.json())
+    .then(addedMemberData => {
+        const addedMember = `<tr><td>${addedMemberData.username}</td><td>${addedMemberData.email}</td><td>${addedMemberData.firstName + " " + addedMemberData.lastName}</td></tr>`
+        document.getElementById("tbody_addedMember").innerHTML = addedMember
+    })
+}
+document.getElementById("addmemberbtn").onclick = () => addMember();
+
+function addCar() {
+    const createdCar = {
+        method: "POST",
+        headers: { "Content-type": "application/json" },
+        body: JSON.stringify({
+            brand: document.getElementById("inputBrand").value,
+            model: document.getElementById("inputModel").value,
+            pricePrDay: document.getElementById("inputPrice").value,
+            bestDiscount: document.getElementById("inputDiscount").value
+        })
+    }
+    fetch(URL_CAR, createdCar)
+    .then(r => r.json())
+    .then(addedCarData => {
+        const addedCar = `<tr><td>${addedCarData.id}</td><td>${addedCarData.brand}</td><td>${addedCarData.model}</td><td>${addedCarData.pricePrDay + " DKK"}</td><td>${addedCarData.bestDiscount + " DKK"}</td></tr>`
+        document.getElementById("tbody_addedCar").innerHTML = addedCar
+    })
+}
+document.getElementById("addcarbtn").onclick = () => addCar();
+
+
